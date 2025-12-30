c88909dd8bd3c51fdc2fc43c35135a14a8691233
SantaElena89 <72125860+SantaElena89@users.noreply.github.com>
2025-12-30 09:06:25 +0100

    updated js

--- src/script.js
+++ src/script.js
@@ -14,7 +14,7 @@ function getRecipe(event) {
   let apiKey = "fobd5f34d0a71b340a77389t18db1bc0";
   let context =
     "You are a world class chef that creates delicious healthy recipes with any ingredients. Create it in a step by step format in basic HTML. Don't say it's in HTML in the text. Sign it at the bottom `Your SheCodes AI` in a <strong> format";
-  let prompt = `Create a recipe with the following ingredients ${instructionsInput.value}`;
+  let prompt = `Create a short and easy recipe with the following ingredients ${instructionsInput.value}`;
   let instructions = instructionsInput.value; // Store the value before clearing
   instructionsInput.value = ""; // Clear the input field
 
