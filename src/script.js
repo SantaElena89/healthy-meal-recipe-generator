a8c424ea74ce72450eced2d78d93341d941aa9a3
SantaElena89 <72125860+SantaElena89@users.noreply.github.com>
2025-12-30 09:02:21 +0100

    added animation to generating

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
 function showRecipe(response) {
-  new Typewriter("#recipe-result", {
+  new Typewriter("#recipe", {
     strings: "🍲 Here's your recipe:" + response.data.answer,
     autoStart: true,
     delay: 1,
@@ -10,14 +10,19 @@ function showRecipe(response) {
 function getRecipe(event) {
   event.preventDefault();
 
-  let instructionsInput = document.querySelector(".instructions");
+  let instructionsInput = document.querySelector("#instructions");
   let apiKey = "fobd5f34d0a71b340a77389t18db1bc0";
   let context =
     "You are a world class chef that creates delicious healthy recipes with any ingredients. Create it in a step by step format in basic HTML. Don't say it's in HTML in the text. Sign it at the bottom `Your SheCodes AI` in a <strong> format";
   let prompt = `Create a recipe with the following ingredients ${instructionsInput.value}`;
-  instructionsInput.value = "";
+  let instructions = instructionsInput.value; // Store the value before clearing
+  instructionsInput.value = ""; // Clear the input field
 
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let recipeElement = document.querySelector("#recipe");
+  recipeElement.classList.remove("hidden");
+  recipeElement.innerHTML = `<div class="generating">👩‍🍳 Cooking your recipe with ${instructions}</div>`;
 
   axios.get(apiURL).then(showRecipe);
 }
